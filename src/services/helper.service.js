6836c509fe427e43304141f5e44d02577ace7e65
Gulshan Kumar <74947700+gulshank99@users.noreply.github.com>
2024-02-02 16:58:13 +0530

    Changed: Link OF end Point of backend

--- src/services/helper.service.js
+++ src/services/helper.service.js
@@ -1,4 +1,5 @@
-export const BASE_URL = `http://localhost:9090`;
+// export const BASE_URL = `http://localhost:9090`;
+export const BASE_URL = `https://techbazaar-production.up.railway.app`;
 export const PRODUCT_PAGE_SIZE = 10;
 export const ADMIN_ORDER_PAGE_SIZE = 10;
 export const USER_PAGE_SIZE = 10;
